ef5bce04e88331ae5a706503412b22971a34fad8
sanjana <sanjanampati6@gmail.com>
2025-08-24 10:57:01 +0530

    Added Background Picture

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,15 +10,15 @@ const App = () => {
   const { authUser } = useContext(AuthContext);
 
   return (
-    <div className="bg-[url('../public/bgImage.svg')] bg-contain">
-      <Toaster />
-      <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-      </Routes>
-    </div>
-  );
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
+  <Toaster />
+  <Routes>
+    <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+    <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+    <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+  </Routes>
+</div>
+  )
 };
 
 export default App;
